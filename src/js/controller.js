7c683ef6d094bd81a051a97826fb28250737a8dd
Polina Lozova <1polinalozovaya1@gmail.com>
2024-03-14 07:17:49 +0300

    add helpers and config files

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,19 +6,11 @@ import recipeView from './views/recipeView.js';
 
 
 
-const timeout = function (s) {
-    return new Promise(function (_, reject) {
-        setTimeout(function () {
-            reject(new Error(`Request took too long! Timeout after ${s} second`));
-        }, s * 1000);
-    });
-};
+
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
-
-
 
 const controlRecipes = async function () {
     try {
--- src/js/model.js
+++ src/js/model.js
@@ -1,13 +1,12 @@
+import { API_URL } from "./config.js";
+import { getJSON } from "./helpers.js";
 export const state = {
     recipe: {},
 };
 
 export const loadRecipe = async function (id) {
-    // try {
-        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
-        const data = await res.json();
-
-        if (!res.ok) throw new Error(`${data.message} (${res.status})`)
+    try {
+        const data = await getJSON(`${API_URL}/${id}`);
 
         const { recipe } = data.data;
         state.recipe = {
@@ -20,7 +19,7 @@ export const loadRecipe = async function (id) {
             cookingTime: recipe.cooking_time,
             ingredients: recipe.ingredients
         };
-    // } catch (err) {
-    //     alert(err)
-    // }
+    } catch (err) {
+        throw err;
+    }
 };
